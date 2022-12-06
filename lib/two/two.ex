55cf82176d5feab072306cdab5dea89b6240e6cd
Scooter Wadsworth <scooter@mix-truck.com>
2022-12-06 11:12:29 -0500

    Run mix format

--- lib/two/two.ex
+++ lib/two/two.ex
@@ -23,40 +23,61 @@ defmodule AOC.Two do
     |> Enum.map(&String.split/1)
   end
 
-  defp get_ideal_play_value("A", 0), do: 3 # Scissors
-  defp get_ideal_play_value("A", 3), do: 1 # Rock
-  defp get_ideal_play_value("A", 6), do: 2 # Paper
+  # Scissors
+  defp get_ideal_play_value("A", 0), do: 3
+  # Rock
+  defp get_ideal_play_value("A", 3), do: 1
+  # Paper
+  defp get_ideal_play_value("A", 6), do: 2
 
-  defp get_ideal_play_value("B", 0), do: 1 # Rock
-  defp get_ideal_play_value("B", 3), do: 2 # Paper
-  defp get_ideal_play_value("B", 6), do: 3 # Scissors
+  # Rock
+  defp get_ideal_play_value("B", 0), do: 1
+  # Paper
+  defp get_ideal_play_value("B", 3), do: 2
+  # Scissors
+  defp get_ideal_play_value("B", 6), do: 3
 
-  defp get_ideal_play_value("C", 0), do: 2 # Paper
-  defp get_ideal_play_value("C", 3), do: 3 # Scissors
-  defp get_ideal_play_value("C", 6), do: 1 # Rock
+  # Paper
+  defp get_ideal_play_value("C", 0), do: 2
+  # Scissors
+  defp get_ideal_play_value("C", 3), do: 3
+  # Rock
+  defp get_ideal_play_value("C", 6), do: 1
 
   defp get_ideal_score("X"), do: 0
   defp get_ideal_score("Y"), do: 3
   defp get_ideal_score("Z"), do: 6
 
-  defp get_play_value("X"), do: 1 # Rock
-  defp get_play_value("Y"), do: 2 # Paper
-  defp get_play_value("Z"), do: 3 # Scissors
+  # Rock
+  defp get_play_value("X"), do: 1
+  # Paper
+  defp get_play_value("Y"), do: 2
+  # Scissors
+  defp get_play_value("Z"), do: 3
 
   # Opponent Plays Rock
-  defp get_score("A", "X"), do: 3 # You play Rock {Rock, Rock} - TIE
-  defp get_score("A", "Y"), do: 6 # You play Paper {Rock, Paper} - WIN
-  defp get_score("A", "Z"), do: 0 # You play Scissors {Rock, Scissors} - LOSE
+  # You play Rock {Rock, Rock} - TIE
+  defp get_score("A", "X"), do: 3
+  # You play Paper {Rock, Paper} - WIN
+  defp get_score("A", "Y"), do: 6
+  # You play Scissors {Rock, Scissors} - LOSE
+  defp get_score("A", "Z"), do: 0
 
   # Opponent Plays Paper
-  defp get_score("B", "X"), do: 0 # You play Rock {Paper, Rock} - LOSE
-  defp get_score("B", "Y"), do: 3 # You play Paper {Paper, Paper} - TIE
-  defp get_score("B", "Z"), do: 6 # You play Scissors {Paper, Scissors} - WIN
+  # You play Rock {Paper, Rock} - LOSE
+  defp get_score("B", "X"), do: 0
+  # You play Paper {Paper, Paper} - TIE
+  defp get_score("B", "Y"), do: 3
+  # You play Scissors {Paper, Scissors} - WIN
+  defp get_score("B", "Z"), do: 6
 
   # Opponent Plays Scissors
-  defp get_score("C", "X"), do: 6 # You play Rock {Scissors, Rock} - WIN
-  defp get_score("C", "Y"), do: 0 # You play Rock {Scissors, Paper} - LOSE
-  defp get_score("C", "Z"), do: 3 # You play Rock {Scissors, Scissors} - TIE
+  # You play Rock {Scissors, Rock} - WIN
+  defp get_score("C", "X"), do: 6
+  # You play Rock {Scissors, Paper} - LOSE
+  defp get_score("C", "Y"), do: 0
+  # You play Rock {Scissors, Scissors} - TIE
+  defp get_score("C", "Z"), do: 3
 
   defp score_ideal_round([opponent_play, desired_result]) do
     ideal_score = get_ideal_score(desired_result)
